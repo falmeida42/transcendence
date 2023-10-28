3c38e1abfc7de1c71cbd4f8d8505e161d226b81a
flavioalmeida <flavio.almeida@ctw.bmwgroup.com>
2023-10-28 19:08:28 +0100

    Backend dockerfile init with docker compose up

--- src/backend/src/app.service.ts
+++ src/backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  get42(): string {
     return '42 Lidsadassbdfsfdsffgddfdoa';
   }
 }
