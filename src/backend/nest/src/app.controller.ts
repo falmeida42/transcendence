31a6e720c149286cbd605d2f38b7e0558650a32d
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2023-11-13 20:46:21 +0000

    removed node_modules

--- src/backend/nest/src/app.controller.ts
+++ src/backend/nest/src/app.controller.ts
@@ -2,14 +2,14 @@ import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
-@Controller("home")
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   get42(@Res() res: Response): string {
-    const responseWellcome = "Wellcome to transcendence #Api response"
+    const responseWellcome = this.appService.get42();
     res.send(responseWellcome);
-    return responseWellcome
+    return responseWellcome;
   }
 }
