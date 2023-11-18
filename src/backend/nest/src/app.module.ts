e162d908a15f9055b3c1a51b58bdc5683e5f2fbc
flavioalmeida <flavio.almeida@ctw.bmwgroup.com>
2023-11-18 17:39:40 +0000

    remove user module

--- src/backend/nest/src/app.module.ts
+++ src/backend/nest/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    UserModule,
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
