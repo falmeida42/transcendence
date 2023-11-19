279fc9a9c3c5afc4e9f2a59badb53be62239cf85
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2023-11-19 23:12:10 +0000

    added fields to User model

---
+++ src/backend/nest/prisma/migrations/20231119230817_extended_user/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "first_name" TEXT,
+ADD COLUMN     "last_name" TEXT,
+ADD COLUMN     "username" TEXT NOT NULL;
