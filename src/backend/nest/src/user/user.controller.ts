3623770cf0f546db5e22e1488a5c6ce1c0dc5169
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2023-11-26 15:49:26 +0000

    user controller

--- src/backend/nest/src/user/user.controller.ts
+++ src/backend/nest/src/user/user.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private usersService: UserService) {}
+
+  @Get('/:id')
+  async findById(@Param('id') id: number) {
+    return this.usersService.getUserById(id);
+  }
+
+  @Post()
+//   @UseGuards()
+  async create(@Body() user: UserDto) {
+    if (!user) {
+      return 'No value inserted';
+    }
+    console.log(user);
+    return this.usersService.create(user);
+  }
+
+  @Delete('/:login')
+//   @UseGuards()
+  async delete(@Param('login') login: string) {
+    if (!login) {
+      return 'No value inserted';
+    }
+    return this.usersService.delete(login);
+  }
+}
