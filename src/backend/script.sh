e96345227be510d2fa576e09c4e7614c102e72e8
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2023-12-03 11:09:17 +0000

    minor stuff

--- src/backend/script.sh
+++ src/backend/script.sh
@@ -2,11 +2,9 @@
 
 # Install app dependencies
 npm i
-# Generate prisma client, leave out if generating in `postinstall` script
-npx prisma generate
 
 npm i cors
 
-npx prisma migrate deploy
+npx prisma generate
 
 exec "$@"
