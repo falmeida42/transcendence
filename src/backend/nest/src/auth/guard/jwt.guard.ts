2f89f63b5881aee5b9f052e056633a1d93ef8921
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2023-12-17 11:00:55 +0000

    correcting jwt guard

--- src/backend/nest/src/auth/guard/jwt.guard.ts
+++ src/backend/nest/src/auth/guard/jwt.guard.ts
@@ -7,14 +7,10 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor() {
     super();
   }
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log('JwtAuthGuard');
-    // Add your custom logic to check if the user is authenticated
-    const request = context.switchToHttp().getRequest();
-    if (request.isAuthenticated()) {
-      return super.canActivate(context);
-    }
+    return super.canActivate(context);
   }
 }
