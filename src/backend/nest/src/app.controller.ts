ae5a46ff216478471dc74f57a92431ecb2fc5a88
Joao Manuel Aiveca Caseiro <jaiveca-@c2r1s5.42lisboa.com>
2023-12-27 13:32:22 +0000

    cleanup

--- src/backend/nest/src/app.controller.ts
+++ src/backend/nest/src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 import { FTGuard, JwtAuthGuard } from './auth/guard';
 import { AuthService } from './auth/auth.service';
 
-@Controller('')
+@Controller('home')
 export class AppController {
   constructor(
     private readonly appService: AppService,
