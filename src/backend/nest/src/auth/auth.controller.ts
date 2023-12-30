e654e4b4ed37afd279da61c6f2fb83bf505e043b
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2023-12-30 18:31:45 +0000

    one time token logic for 2fa

--- src/backend/nest/src/auth/auth.controller.ts
+++ src/backend/nest/src/auth/auth.controller.ts
@@ -12,11 +12,10 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { FTGuard, JwtAuthGuard } from '../auth/guard';
-import { AuthDto } from './dto';
 import { UserService } from '../user/user.service';
 import { GetMe } from 'src/decorators';
 import { User } from '@prisma/client';
-import { UserDto } from 'src/user/dto';
+import { twoFAGuard } from './guard/2FA.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -36,21 +35,19 @@ export class AuthController {
   async callbackIntra(@Req() req: any, @Res() res: any): Promise<any> {
     this.logger.debug('Request user:', req.user);
 
-    const twoFactorEnabled = await this.authService.is2FAActive(
-      String(req.user.id),
-    );
-    if (twoFactorEnabled) {
+    if (await this.authService.is2FAActive(String(req.user.id))) {
       this.logger.debug('2FA IS ENABLED');
-
-      // TODO: need to validate with QR code instead
 
       const user = this.userService.getUserById(req.user.id);
       if (!user) {
         throw new ForbiddenException('User not found');
       }
 
-      return this.register(res, req);
+      // TODO: send to frontend here
+      const token2fa = await this.authService.sign2FAToken(req.user.id);
+      return res.redirect(`${process.env.FRONTEND_URL}/?token2fa=${token2fa}`);
     }
+
     const data = await this.authService.signup(req.user);
     this.logger.debug('Token:', data.token);
 
@@ -58,123 +55,104 @@ export class AuthController {
   }
 
   // For testing purposes
-  @Post('signin-test')
-  signin(@Body() dto: AuthDto) {
-    this.logger.debug(dto);
+  // @Post('signin-test')
+  // signin(@Body() dto: AuthDto) {
+  //   this.logger.debug(dto);
 
-    return this.authService.signup(dto);
-  }
+  //   return this.authService.signup(dto);
+  // }
 
-  @Post('2fa/generate')
-  async register(@Res() res: any, @Req() @Body() req: any) {
-    console.log('Request: ', req);
+  @UseGuards(JwtAuthGuard)
+  @Get('2fa/generate')
+  async register(@Res() res: any, @GetMe() user: User) {
     this.logger.debug(res.user);
 
     // generate 2FA secret
     const secret = await this.authService.generate2FASecret();
     // generate key uri
-    const otpAuthURL = await this.authService.generate2FAKeyURI(
-      req.user,
-      secret,
-    );
+    const otpAuthURL = await this.authService.generate2FAKeyURI(user, secret);
 
     this.logger.debug('secret: ', secret);
     this.logger.debug('otp url: ', otpAuthURL);
 
     // update user data
-    const user = await this.userService.set2FASecret(
-      String(req.user.id),
-      secret,
-    );
-    if (!user) {
-      this.logger.error('User not updated');
+    if (await this.userService.set2FASecret(String(user.id), secret)) {
+      // generate QR code
+      return res.json(await this.authService.generateQrCodeURL(otpAuthURL));
     }
-    // generate QR code
-    return res.json(await this.authService.generateQrCodeURL(otpAuthURL));
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('2fa/turn-on')
-  async turn2FAOn(@Body() body: any) {
-    this.logger.debug(body.user);
+  async turn2FAOn(
+    @GetMe() user: User,
+    @Body('code') code: string,
+    @Res() res: any,
+  ) {
+    this.logger.debug(user);
 
-    if (
-      (await this.userService.is2FAEnabled(body.user.id)).valueOf() === false
-    ) {
-      const isCodeValid = this.authService.is2FACodeValid(
-        body.twoFactorAuthCode,
-        body.user,
-      );
+    if ((await this.userService.is2FAEnabled(user.id)).valueOf() === false) {
+      // TODO: Check if 2FA has been generated before, generate if not
+      // TODO: Send code to frontend
+
+      const isCodeValid = this.authService.is2FACodeValid(code, user);
 
       if (!isCodeValid) {
         throw new UnauthorizedException('Wrong 2FA code');
       }
 
-      await this.userService.set2FAOn(body.user.id);
-      // return await this.authenticate2FA(body); // Do I really need to authenticate here??
+      await this.userService.set2FAOn(user.id);
+      return await this.authenticate2FA(code, user.id, res); // Do I really need to authenticate here??
     }
     return { message: '2FA is already on' };
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('2fa/turn-off')
-  async turn2FAOff(@Req() req: any, @Body() body: any) {
+  async turn2FAOff(@GetMe() user: User, @Body() body: any, @Res() res: any) {
     if ((await this.userService.is2FAEnabled(body.user.id)) === true) {
       const isCodeValid = this.authService.is2FACodeValid(
         body.twoFactorAuthenticationCode,
-        req.user,
+        user,
       );
 
       if (!isCodeValid) {
         throw new UnauthorizedException('Wrong 2FA code');
       }
 
-      await this.userService.set2FAOff(req.user.id);
-      // return await this.authenticate2FA(body); // Do I really need to authenticate here??
+      const authenticated = await this.authenticate2FA(body, user.id, res);
+      if (!authenticated) {
+        throw new ForbiddenException('User not authenticated');
+      }
+      await this.userService.set2FAOff(user.id);
     }
     return { message: '2FA is already off' };
   }
 
-  // TODO: Use 2fa specific guard maybe
-  // // @UseGuards(twoFAGuard)
-  // @Post('2fa/authentication')
-  // async authenticate2FA(
-  //   // @GetMe() user: User,
-  //   @Body() body: any,
-  //   @Res() res: any,
-  // ) {
-  //   const codeValid = await this.authService.is2FACodeValid(
-  //     body.twoFactorAuthenticationCode,
-  //     body.user,
-  //   );
+  @UseGuards(twoFAGuard)
+  @Post('2fa/authentication')
+  async authenticate2FA(
+    @Body('code') code: string,
+    @Body('id') id: string,
+    @Res() res: any,
+  ) {
+    this.logger.debug('code: ', code);
 
-  //   if (!codeValid) {
-  //     throw new UnauthorizedException('Wrong 2FA code');
-  //   }
+    const user = await this.userService.getUserById(id);
 
-  //   // TODO: sign token here
-  //   // this.authService.signup()
+    if (!user) {
+      throw new ForbiddenException('No such id ', user.id);
+    }
 
-  //   // TODO: redirect to home page
-  //   return res.redirect(`${process.env.FRONTEND_URL}/?token=${body.token}`);
-  // }
-
-  @Get('2fa/:user')
-  async signToken(@Body() dto: UserDto, @Res() res: any) {
-    this.logger.debug('User: ', dto);
-
-    // TODO: validate 2FA
-    // const isCodeValid = this.authService.is2FACodeValid(
-    //   body.twoFactorAuthenticationCode,
-    //   dto,
-    // );
+    const isCodeValid = this.authService.is2FACodeValid(code, user);
 
     if (!isCodeValid) {
       throw new UnauthorizedException('Wrong 2FA code');
     }
 
-    const data = await this.authService.signup(dto);
-    this.logger.log('Token: ', data.token);
-    return res.redirect(`${process.env.FRONTEND_URL}/?token=${data.token}`);
+    const token = await this.authService.signToken(Number(user.id), user.login);
+
+    this.logger.log('Token: ', token);
+    return res.redirect(`${process.env.FRONTEND_URL}/?token=${token}}`);
   }
 }
