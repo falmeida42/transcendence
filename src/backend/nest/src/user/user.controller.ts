cf0e235bbfa61d462230117c24537f78bcdc6894
fbarros <fbarros@student.42lisboa.com>
2024-01-19 21:56:05 +0000

    image updating immediately

--- src/backend/nest/src/user/user.controller.ts
+++ src/backend/nest/src/user/user.controller.ts
@@ -56,17 +56,9 @@ export class UserController {
   @Post('me')
   async updateMe(
     @GetMe('id') id: string,
-    @Body() userData: UserDto,
-    @Res() res: Response,
+    @Body() userData: any,
   ) {
-    if (!userData) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ message: 'Empty body' })
-        .send();
-    }
-    const updatedUser = await this.userService.updateUserById(id, userData);
-    return updatedUser;
+    return await this.userService.updateUserById(id, userData);
   }
 
   @UseGuards(TwoFAGuard)
