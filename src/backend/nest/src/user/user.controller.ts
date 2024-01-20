8d9907dae93d88a06094a28fb2b8abd48a601fe7
franciscobarros <francisco.barros@ctw.bmwgroup.com>
2024-01-20 17:31:08 +0000

    re-adding pipes to controller

--- src/backend/nest/src/user/user.controller.ts
+++ src/backend/nest/src/user/user.controller.ts
@@ -94,10 +94,9 @@ export class UserController {
 
   @UseGuards(JwtAuthGuard)
   @Get('friends/:id')
-  async getFriendfriend(@Param('id') id: string) {
+  async getFriendfriend(@Param('id', InputStringValidationPipe) id: string) {
     const user = await this.userService.getFriends(id);
-    if (user)
-      return user;
+    if (user) return user;
   }
 
   @UseGuards(JwtAuthGuard)
@@ -502,8 +501,8 @@ export class UserController {
   @Post('mute-user')
   async muteUser(
     @GetMe() user: User,
-    @Body('participantId') participantId: string,
-    @Body('roomId') roomId: string,
+    @Body('participantId', InputStringValidationPipe) participantId: string,
+    @Body('roomId', InputStringValidationPipe) roomId: string,
     @Body('duration', ParseIntPipe) duration: number,
     @Res() res: Response,
   ) {
@@ -547,8 +546,8 @@ export class UserController {
 
   @Get('check-mute/:userId/:roomId')
   async checkUserMute(
-    @Param('userId') userId: string,
-    @Param('roomId') roomId: string,
+    @Param('userId', InputStringValidationPipe) userId: string,
+    @Param('roomId', InputStringValidationPipe) roomId: string,
   ) {
     const isMuted = await this.userService.isUserMutedInRoom(userId, roomId);
     if (!isMuted) {
@@ -561,8 +560,8 @@ export class UserController {
   @Post('unmute-user')
   async unmuteUser(
     @GetMe() user: User,
-    @Body('participantId') participantId: string,
-    @Body('roomId') roomId: string,
+    @Body('participantId', InputStringValidationPipe) participantId: string,
+    @Body('roomId', InputStringValidationPipe) roomId: string,
     @Res() res: Response,
   ) {
     if (!roomId || !participantId) {
@@ -587,7 +586,7 @@ export class UserController {
   @Post('update-room-privacy/:roomId')
   async updateRoomPrivacy(
     @Body('type') type: any,
-    @Body('password') password: string,
+    @Body('password', InputStringValidationPipe) password: string,
     @Param('roomId', InputStringValidationPipe) roomId: string,
   ) {
     if (type && password) {
